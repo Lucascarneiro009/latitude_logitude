8da3c4c89c9302354830df3c8dd0e6943e4343ba
mateusp55 <mateus.smd@gmail.com>
2020-10-22 21:36:28 -0300

    novo teste

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 # Testando a conversão do CEP e número 
 
-# teste - Mateus está aprendendo como usar o git
+# teste - Mateus está aprendendo como usar o git - Mais um teste
 
 from LatLong.LatLongApi import LatLongApi
 
