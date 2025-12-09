5e7cba7365bf8099bf6fa6a88bccb37414c93d8b
Lucas Carneiro <54177832+Lucascarneiro009@users.noreply.github.com>
2025-12-08 22:46:43 -0300

    Ajustando Endereço

--- teste.py
+++ teste.py
@@ -5,6 +5,6 @@
 from LatLong.LatLongApi import LatLongApi
 
 ll = LatLongApi()
-lat, lon = ll.consultar(cep = 60160150, numero = 156)
+lat, lon = ll.consultar(cep = 99999999, numero = 111)
 
 print('A latitude é {0} e a longitude é {1}!'.format(lat, lon))
